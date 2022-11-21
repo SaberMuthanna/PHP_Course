5c39c1ec698a58ae4f2cf2a28d5dc68501615710
Saber Muthanna <34119145+SaberMuthanna@users.noreply.github.com>
2022-11-21 18:34:45 +0300

    file comments

---
+++ 2. Comments.php
@@ -0,0 +1,24 @@
+
+<?php
+
+// Single Line Comment
+echo 'Test'; // Single Line Comment
+echo 'Test'; # Single Line Comment
+echo '<br>';
+/*
+    Line 1
+    Line 2
+  */
+
+/*
+    ====================
+    == My Special App ==
+    ====================
+  */
+
+/* Single Line */
+
+echo 'Test' /* Single Line */;
+
+// I Used Print Because Its Faster Than Echo <= Example Not True
+print 'Test';
